91cd399a0fad8a7690acde0a5a1c26f6c4129831
Alex Walker <alexjpwalker@gmail.com>
2023-01-19 16:35:45 +0000

    Add "crate-name={}" tag to Crate Universe targets (#1787)
    When interoperating with Cargo, it's useful to have knowledge of the original crate name for a crate downloaded via Crate Universe (e.g: for distributing a crate compiled via Bazel on crates.io).
    
    Unfortunately this name is not currently exposed anywhere, besides (incidentally) via the auto-generated repository name: `@crates__async-trait-0.36.0` (for the target `@crates__async-trait-0.36.0//:async_trait`) and via the `alias`, `@crates//:async-trait`, which (to the best of my knowledge) is not generally visible to a Bazel aspect, as the `target` field in an aspect implementation refers to the original target being aliased - in this case `@crates__async-trait-0.36.0//:async_trait`
    
    As a workaround it is possible to retrieve the crate name through string parsing, but this feels like a hack. In this PR we add a tag to each Crate Universe target: for example `"crate-name=async-trait"`.
    
    Fixes #1783 .

--- crate_universe/src/rendering.rs
+++ crate_universe/src/rendering.rs
@@ -379,6 +379,7 @@ impl Renderer {
                 tags.insert("manual".to_owned());
                 tags.insert("noclippy".to_owned());
                 tags.insert("norustfmt".to_owned());
+                tags.insert(format!("crate-name={}", krate.name));
                 tags
             },
             tools: attrs
@@ -514,6 +515,7 @@ impl Renderer {
                 tags.insert("manual".to_owned());
                 tags.insert("noclippy".to_owned());
                 tags.insert("norustfmt".to_owned());
+                tags.insert(format!("crate-name={}", krate.name));
                 tags
             },
             version: krate.common_attrs.version.clone(),
@@ -766,6 +768,7 @@ mod test {
 
         assert!(build_file_content.contains("rust_library("));
         assert!(build_file_content.contains("name = \"mock_crate\""));
+        assert!(build_file_content.contains("\"crate-name=mock_crate\""));
     }
 
     #[test]
@@ -797,6 +800,7 @@ mod test {
 
         assert!(build_file_content.contains("cargo_build_script("));
         assert!(build_file_content.contains("name = \"build_script_build\""));
+        assert!(build_file_content.contains("\"crate-name=mock_crate\""));
 
         // Ensure `cargo_build_script` requirements are met
         assert!(build_file_content.contains("name = \"mock_crate_build_script\""));
@@ -825,6 +829,7 @@ mod test {
 
         assert!(build_file_content.contains("rust_proc_macro("));
         assert!(build_file_content.contains("name = \"mock_crate\""));
+        assert!(build_file_content.contains("\"crate-name=mock_crate\""));
     }
 
     #[test]
@@ -850,6 +855,7 @@ mod test {
 
         assert!(build_file_content.contains("rust_binary("));
         assert!(build_file_content.contains("name = \"mock_crate__bin\""));
+        assert!(build_file_content.contains("\"crate-name=mock_crate\""));
     }
 
     #[test]
